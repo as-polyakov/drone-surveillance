ab4b0a1260b33c871168c6cb38d0abc23156632c
Anton Polyakov <polyakov.anton@gmail.com>
2025-05-13 12:06:03 -0700

    logging

--- video.py
+++ video.py
@@ -1,4 +1,4 @@
-import logging as log
+import logging
 import datetime as dt
 from time import sleep
 
@@ -7,14 +7,16 @@ import cv2 as cv
 
 cascPath = cv.data.haarcascades + "haarcascade_frontalface_default.xml"
 faceCascade = cv.CascadeClassifier(cascPath)
-log.basicConfig(filename='webcam.log',level=log.INFO)
-
+logger = logging.getLogger(__name__)
+logging.basicConfig(filename='webcam.log',level=logging.INFO)
 def capture_frame():
     #cv.namedWindow("test")
     cam = cv.VideoCapture(0, cv.CAP_AVFOUNDATION)
+    logger.info("Initializing webcam")
     while not cam.isOpened():
         sleep(1)
 
+    logger.info("Webcam initialized")
     while True:
         ret, frame = cam.read()
         if not ret:
@@ -42,6 +44,7 @@ def detect_objects(frame):
     for (x, y, w, h) in faces:
         cv.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
     if len(faces) > 0:
+        print("Found " + str(len(faces)) + " faces")
         cv.imwrite('objects-' + dt.datetime.now().strftime("%d-%m-%Y-%H:%M:%S") + ".png", frame)
 
 if __name__ == "__main__":
